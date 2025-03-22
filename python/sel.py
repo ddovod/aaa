7f83056d8d468836906d1805d77413158f10c219
ddovod <ddovod@gmail.com>
2025-03-23 00:31:06 +0300

    some

--- python/sel.py
+++ python/sel.py
@@ -4,24 +4,20 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 
 
-def exists(driver, by, path):
-    return len(driver.find_elements(by, path)) != 0
-
-def visible(driver, by, path):
-    return driver.find_element(by, path).is_displayed()
-
-def get(driver, by, path):
-    return driver.find_element(by, path)
-
 def get_if_visible(driver, by, path):
-    els = driver.find_elements(by, path)
-    if len(els) == 1:
-        el = els[0]
-        if el.is_displayed():
-            return el
-    elif len(els) > 1:
-        log.warning("Several '" + path + "' found")
+    try:
+        els = driver.find_elements(by, path)
+        if len(els) == 1:
+            el = els[0]
+            if el.is_displayed():
+                return el
+        elif len(els) > 1:
+            log.warning("Several '" + path + "' found")
+    except Exception:
+        pass
+
     return None
+
 
 def get_bid_if_exists(driver, by, path):
     try:
@@ -32,6 +28,7 @@ def get_bid_if_exists(driver, by, path):
         pass
 
     return None
+
 
 def get_time_seconds_if_exists(driver, by, path):
     try:
