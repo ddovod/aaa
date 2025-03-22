9ab745a1d78c3a87a9ed4352f7fbb542b7b8f92e
ddovod <ddovod@gmail.com>
2025-03-23 00:15:39 +0300

    some

--- python/sel.py
+++ python/sel.py
@@ -24,24 +24,26 @@ def get_if_visible(driver, by, path):
     return None
 
 def get_bid_if_exists(driver, by, path):
-    bid_txt = get_if_visible(driver, by, path)
-    if bid_txt != None:
-        try:
+    try:
+        bid_txt = get_if_visible(driver, by, path)
+        if bid_txt != None:
             return int(re.sub("[^0-9]", "", bid_txt.text))
-        except Exception:
-            pass
+    except Exception:
+        pass
+
     return None
 
 def get_time_seconds_if_exists(driver, by, path):
-    time_left_txt = get_if_visible(driver, by, path)
-    if time_left_txt != None:
-        hms = time_left_txt.text
-        if hms != None:
-            comps = hms.split(':')
-            if len(comps) == 3:
-                try:
+    try:
+        time_left_txt = get_if_visible(driver, by, path)
+        if time_left_txt != None:
+            hms = time_left_txt.text
+            if hms != None:
+                comps = hms.split(':')
+                if len(comps) == 3:
                     return int(comps[0]) * 60 * 60 + int(comps[1]) * 60 + int(comps[2])
-                except Exception:
-                    pass
+    except Exception:
+        pass
+
     return None
 
